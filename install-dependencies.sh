96879de7a49e51c258f981e1ab838d682c95c0b6
Kevin Cunningham <kevin.cunningham@carta.com>
2022-08-31 11:47:11 -0400

    install additional dependencies

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,1 +1,9 @@
 # Add commands to install any dependencies
+
+sudo apt update && sudo apt install gnupg2 -y
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $OH_MY_ZSH_PLUGINS_PATH/zsh-syntax-highlighting
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+vim +PlugInstall +qa
